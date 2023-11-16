714e763d1f4dd0e9b8438438ae653cb699920a61
davy lebris <davylebris@gmail.com>
2023-11-16 09:42:28 +0100

    mise en place de la partie back-end

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 8001;
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`Server run on ${port}`);
+});
